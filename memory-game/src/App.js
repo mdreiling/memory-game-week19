9fd1f15c8413c58d6ee81846d5c6d1d0dd29f264
unknown <dreilingmarshall@gmail.com>
2019-04-04 18:37:29 -0400

    Cotinuted work on reset. Restructuring of isClicked logic

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -3,37 +3,53 @@ import FlagCard from "./components/FlagCard";
 import Wrapper from "./components/Wrapper";
 import flags from "../src/flags.json";
 
+const allFlags = flags;
+
 class App extends Component {
   state = {
-    flags,
-    score: 0
+    allFlags,
+    score: 0,
+    selectedFlags: []
   };
 
   // Function to randomize cards that are being shown
-  // randomCard = id => {
-  //   const flags = this.state.flags;
-  //     this.setState({flags});
-  // };
+  randomFlag = allFlags => {
+    // console.log(allFlags);
+    for (var i = 0; i < 16; i++) {
+        this.state.selectedFlags.push(allFlags[Math.floor(Math.random()*allFlags.length)]);
+    }
+    console.log(this.state.selectedFlags);
+  };
 
   // Function to show a card has been clicked
   handleClick = clickedCard => {
-    if (!clickedCard.state.isClicked) {
-      clickedCard.setState({ isClicked: true });
+    // console.log(clickedCard);
+    if (!clickedCard.isClicked) {
+      // clickedCard.isClicked = true;
+      console.log(clickedCard.isClicked);
       this.handleScore();
     } else {
       this.gameReset();
     }
   };
 
+  // Function to update the score when a previously unclicked flag is clicked
   handleScore = () => {
     this.setState({ score: this.state.score + 1 });
   };
 
+  // Function to reset the game when a flag is clicked for the second time
   gameReset = () => {
-    console.log("Game over");
+    alert("Game over");
     this.setState({ score: 0 });
+    console.log(this);
   }
 
+  // Calling randomFlag function to initialize the game
+  componentDidMount() {
+    this.randomFlag(allFlags);
+    console.log(this);
+  }
 
   // Render function 
   render() {
@@ -42,9 +58,10 @@ class App extends Component {
         <h2 className="title">World Flag Memory Game</h2>
         <h4 className="score">{this.state.score}</h4>
         <div className="container">
-          {this.state.flags.map((flag) => (
+          {this.state.allFlags.map((flag) => (
             <FlagCard
               handleClick = {this.handleClick}
+              isClicked = {false}
               id = {flag.id}
               key = {flag.id}
               name = {flag.name}
--- memory-game/src/components/FlagCard/index.js
+++ memory-game/src/components/FlagCard/index.js
@@ -1,40 +1,15 @@
-import React, {Component} from "react";
+import React from "react";
 
-class FlagCard extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            id: this.props.id,
-            isClicked: false
-        };
-
-        // this.handleClick = this.handleClick.bind(this.FlagCard);
-        // this.updateScore = this.updateScore.bind(this);
-
-        // console.log(this.state);
-    }
-
-    // handleClick(clickedCard) {
-    //     clickedCard.setState({ isClicked: true });
-    //     console.log(clickedCard.state);
-    // }
-
-    render() {
-        return (
-            <div className="col s12 m3">
-                <div className="card" key={this.props.id} onClick={() => this.props.handleClick(this)}>
-                    <div className="card-image">
-                        <img className="responsive-img" alt={this.props.name} src={this.props.image} />
-                    </div>
+function FlagCard(props) {
+    return (
+        <div className="col s12 m3">
+            <div className="card" key={props.id} onClick={() => props.handleClick(props)}>
+                <div className="card-image">
+                    <img className="responsive-img" alt={props.name} src={props.image} data-clicked={props.isClicked} />
                 </div>
             </div>
-        );
-    }
-
-
+        </div>
+    );
 }
-
-
 
 export default FlagCard;
