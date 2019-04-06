ee870e9315f65deb6eb35537837e8f206e641db6
unknown <dreilingmarshall@gmail.com>
2019-04-06 14:58:57 -0400

    Working game logic and randomizer

--- memory-game/src/components/FlagCard/index.js
+++ memory-game/src/components/FlagCard/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function FlagCard(props) {
     return (
         <div className="col s6 m3">
-            <div className="card" key={props.id} onClick={() => props.handleClick(props)}>
+            <div className="card" key={props.id} id={props.id} onClick={() => props.handleClick(props)}>
                 <div className="card-image">
                     <img className="responsive-img" alt={props.name} src={props.image} data-clicked={props.isClicked} />
                 </div>
